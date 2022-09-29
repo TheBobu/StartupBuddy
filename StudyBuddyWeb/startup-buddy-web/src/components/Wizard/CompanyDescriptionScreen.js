1bae36f251c6f01a74ac14537aad006f0347b6f9
Ana-Maria Aftenie <ana-maria.aftenie@nagarro.com>
2022-09-30 01:13:42 +0300

    Add css, translations, validations and other fields for all screens.

--- StudyBuddyWeb/startup-buddy-web/src/components/Wizard/CompanyDescriptionScreen.js
+++ StudyBuddyWeb/startup-buddy-web/src/components/Wizard/CompanyDescriptionScreen.js
@@ -8,16 +8,17 @@ import { Grid, MenuItem } from "@mui/material";
 import Controls from "../Controls/Controls"
 import * as Yup from "yup";
 import i18n from "i18next";
+import classes from '../Wizard/Wizard.module.css';
 
 const ValidationSchema = () => {
     return Yup.object().shape({
       Name: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required(i18n.t("General.RequiredField")),
       Domain: Yup.number().required(i18n.t("General.RequiredField")),
       MainActivity: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required(i18n.t("General.RequiredField")),
+      SecondaryActivity: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required(i18n.t("General.RequiredField")),
       Description: Yup.string().min(2, "Too Short!").max(100, "Too Long!").required(i18n.t("General.RequiredField")),
       ExperienceLevel: Yup.number().required(i18n.t("General.RequiredField")),
       NumberOfEmployees: Yup.number().required(i18n.t("General.RequiredField")),
-      Members: Yup.string().min(2, "Too Short!").max(100, "Too Long!").required(i18n.t("General.RequiredField")),
       BusinessEmail: Yup.string().min(2, "Too Short!").max(100, "Too Long!").required(i18n.t("General.RequiredField")),
       Phone: Yup.number().required(i18n.t("General.RequiredField")),
       CUI: Yup.number().required(i18n.t("General.RequiredField")),
@@ -31,10 +32,10 @@ const ValidationSchema = () => {
     Name: "",
     Domain: "",
     MainActivity: "",
+    SecondaryActivity: "",
     Description: "",
     ExperienceLevel: "",
     NumberOfEmployees: "",
-    Members: "",
     BusinessEmail: "",
     Phone: "",
     CUI: "",
@@ -45,14 +46,17 @@ const ValidationSchema = () => {
 
   const CompanyDescriptionScreen = () => {
     const { t } = useContext(TranslationContext);
+    const [isAccountantChecked, setIsAccountantChecked] = useState(false);
     const [isCompanyChecked, setIsCompanyChecked] = useState(false);
+    const [isStockMarketChecked, setIsStockMarketChecked] = useState(false);
+    const [isEmployeesChecked, setIsEmployeesChecked] = useState(false);
     const submitHandler = (values) => {
         console.log(values);
     }
     return (
-    <Card>
+    <Card className={classes.wizard_container}>
       <CardContent>
-        <h1>Hello World</h1>
+        <h1>Company Description</h1>
         <Formik
         initialValues={InitialValues}
         validationSchema={ValidationSchema}
@@ -61,18 +65,21 @@ const ValidationSchema = () => {
             <Grid container sx={{ mt: 4 }}>
               <Grid item xs={12}>
                 <Box paddingBottom={3} sx={{ mr: 2 }}>
+                <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.Name)}
-                    placeholder="Name"
+                    label={t('CompanyDescriptionScreen.Name')}
+                    placeholder={t('CompanyDescriptionScreen.Name')}
                     component={TextField}
                     name="Name"
                   ></Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.Domain)}
+                    label={t('CompanyDescriptionScreen.Domain')}
                     name="Domain"
-                    placeholder={t(CompanyDescriptionScreen.Domain)}
+                    placeholder={t('CompanyDescriptionScreen.Domain')}
                     component={Select}
                   >
                     <MenuItem value={1}>Societatea cu Raspundere Limitata (SRL)</MenuItem>
@@ -80,113 +87,178 @@ const ValidationSchema = () => {
                     <MenuItem value={3}>Societatea in nume colectiv (SNC)</MenuItem>
                     <MenuItem value={4}>Societatea in comandita simpla (SCS)</MenuItem>
                     <MenuItem value={5}>Societatea in comandita pe actiuni (SCA)</MenuItem>
-                    <MenuItem value={6}>Grupul de Interes Economic (GIE)</MenuItem>
-                    <MenuItem value={7}>Grupul European de Interes Economic (GEIE)</MenuItem>
-                    <MenuItem value={8}>Societatea Europeana (SE)</MenuItem>
-                    <MenuItem value={9}>Reprezentanta</MenuItem>
+                    <MenuItem value={6}>Persoana fizica autorizata (PFA)</MenuItem>
+                    <MenuItem value={7}>Intreprindere individuala (II)</MenuItem>
+                    <MenuItem value={8}>Organizatie non-guvernamentala (ONG)</MenuItem>
                   </Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.MainActivity)}
-                    placeholder="MainActivity"
+                    label={t('CompanyDescriptionScreen.MainActivity')}
+                    placeholder={t('CompanyDescriptionScreen.MainActivity')}
                     component={TextField}
                     name="MainActivity"
                   ></Field>
+                  <a href="https://caen.ro">Coduri CAEN</a>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.Description)}
-                    placeholder="Description"
+                    label={t('CompanyDescriptionScreen.SecondaryActivity')}
+                    placeholder={t('CompanyDescriptionScreen.SecondaryActivity')}
+                    component={TextField}
+                    name="SecondaryActivity"
+                  ></Field>
+                  </div>
+                  <div className={classes.field}>
+                  <Field
+                    style={{ width: "100%" }}
+                    label={t('CompanyDescriptionScreen.Description')}
+                    placeholder={t('CompanyDescriptionScreen.Description')}
                     component={TextField}
                     name="Description"
                   ></Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.ExperienceLevel)}
-                    placeholder="ExperienceLevel"
+                    label={t('CompanyDescriptionScreen.ExperienceLevel')}
+                    placeholder={t('CompanyDescriptionScreen.ExperienceLevel')}
                     name="ExperienceLevel"
                     component={Select}
                   >
                     <MenuItem value={1}>Beginner</MenuItem>
                     <MenuItem value={2}>Advanced</MenuItem>
                   </Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.NumberOfEmployees)}
-                    placeholder="NumberOfEmployees"
+                    label={t('CompanyDescriptionScreen.Accountant')}
+                    name="Accountant"
+                    component={Checkbox}
+                    value={isAccountantChecked}
+                    onChange={e=>{setIsAccountantChecked(e.target.checked)}}
+                    
+                >
+                  </Field>
+                  </div>
+                  <div className={classes.field}>
+                  <Field
+                    style={{ width: "100%" }}
+                    label={t('CompanyDescriptionScreen.StockMarket')}
+                    name="StockMarket"
+                    component={Checkbox}
+                    value={isStockMarketChecked}
+                    onChange={e=>{setIsStockMarketChecked(e.target.checked)}}
+                    
+                >
+                  </Field>
+                  </div>
+                  <div className={classes.field}>
+                  <Field
+                    style={{ width: "100%" }}
+                    label={t('CompanyDescriptionScreen.Employees')}
+                    name="Employees"
+                    component={Checkbox}
+                    value={isEmployeesChecked}
+                    onChange={e=>{setIsEmployeesChecked(e.target.checked)}}
+                    
+                >
+                  </Field>
+                  </div>
+                  <div className={classes.field}>
+                  <Field
+                    style={{ width: "100%" }}
+                    label={t('CompanyDescriptionScreen.NumberOfEmployees')}
+                    placeholder={t('CompanyDescriptionScreen.NumberOfEmployees')}
                     component={TextField}
                     name="NumberOfEmployees"
                   ></Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.Members)}
-                    placeholder="Members"
-                    component={TextField}
-                    name="Members"
-                  ></Field>
-                  <Field
-                    style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.BusinessEmail)}
-                    placeholder="BusinessEmail"
+                    label={t('CompanyDescriptionScreen.BusinessEmail')}
+                    placeholder={t('CompanyDescriptionScreen.BusinessEmail')}
                     component={TextField}
                     name="BusinessEmail"
                   ></Field>
+                  </div>
+                  <div className={classes.field}>
                     <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.Phone)}
+                    label={t('CompanyDescriptionScreen.Phone')}
                     name="Phone"
-                    placeholder="Phone"
+                    placeholder={t('CompanyDescriptionScreen.Phone')}
                     component={TextField}
                   >
                   </Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label="Founded Company"
-                    name="Founded Company"
+                    label={t('CompanyDescriptionScreen.FoundedCompany')}
+                    name="FoundedCompany"
                     component={Checkbox}
                     value={isCompanyChecked}
                     onChange={e=>{setIsCompanyChecked(e.target.checked)}}
                     
                 >
                   </Field>
+                  </div>
                   {isCompanyChecked && <div>
+                    <div className={classes.field}>
                     <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.CUI)}
-                    name="CUI"
+                    label={t('CompanyDescriptionScreen.CUI')}
+                    name={t('CompanyDescriptionScreen.CUI')}
                     placeholder="CUI"
                     component={TextField}
                   >
                   </Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.DateFounded)}
-                    name="DateFounded"
+                    label={t('CompanyDescriptionScreen.DateFounded')}
+                    name={t('CompanyDescriptionScreen.DateFounded')}
                     placeholder="DateFounded"
                     component={TextField}
                   >
                   </Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.SocialDomain)}
-                    name="SocialDomain"
+                    label={t('CompanyDescriptionScreen.SocialDomain')}
+                    name={t('CompanyDescriptionScreen.SocialDomain')}
                     placeholder="SocialDomain"
                     component={TextField}
                   >
                   </Field>
+                  </div>
+                  <div className={classes.field}>
                   <Field
                     style={{ width: "100%" }}
-                    label={t(CompanyDescriptionScreen.ONRC)}
+                    label={t('CompanyDescriptionScreen.ONRC')}
                     name="ONRC"
-                    placeholder="ONRC"
+                    placeholder={t('CompanyDescriptionScreen.ONRC')}
                     component={TextField}
                   >
                   </Field>
+                  </div>
                   </div> }
                 </Box>
               </Grid>
             </Grid>
             <Grid item xs={12}>
-            <Controls.Button type="submit" text="Submit" />
+              <Controls.Button
+                className={classes.submit_button}
+                type='submit'
+                text={t('General.Next')}
+              />
             </Grid>
           </Form>
         </Formik>
