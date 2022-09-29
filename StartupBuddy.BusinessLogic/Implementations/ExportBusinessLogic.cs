56d75f71237e9bb5cb723a74125586920a456d7b
Andrei Florescu <andrei.florescu.00@gmail.com>
2022-09-30 01:28:49 +0300

    Add pdf pages

--- StartupBuddy.BusinessLogic/Implementations/ExportBusinessLogic.cs
+++ StartupBuddy.BusinessLogic/Implementations/ExportBusinessLogic.cs
@@ -135,7 +135,8 @@ namespace StartupBuddy.BusinessLogic.Implementations
                         <h1>Portofoliu {company.Name}</h1>
                         <h3>Produs: {product.Name}</h3>
                     </div>
-                ";
+                "; 
+            pages.Add(GetHtmlTemplate(titlePage));
 
             var firstPage =
                 @$"
@@ -149,25 +150,61 @@ namespace StartupBuddy.BusinessLogic.Implementations
                         <p>Educație/Formare: {personalInfo.Education}</p>
                     </div>
                 ";
+            pages.Add(GetHtmlTemplate(firstPage));
 
             var secondPage =
                 @$"
                     <div>
                         <h3>Detaliile Companiei</h3>
-                        <p></p>
-                        <p></p>
-                        <p></p>
-                        <p></p>
+                        <p>Activitatea principală: {company.MainActivity}</p>
+                        <p>Activitați secundare: {company.SecondaryActivities}</p>
+                        {(company.Employees? $"<p>Nr. angajați: {company.NumberOfEmployees}</p>":"")}
+                        <p>Email: {company.BusinessEmail}</p>
+                        <p>Descriere: <br>{company.Description}</p>
+                        {(product!=null?$@"
                         <h3>Detaliile Produsului</h3>
-                        <p></p>
-                        <p></p>
-                        <p></p>
+                        <p>Nume: {product.Name}</p>
+                        <p>Tip: {product.ProductType}</p>
+                        <p>Descriere: <br>{product.Description}</p>":"")}
                     </div>
                 ";
-            pages.Add(GetHtmlTemplate(titlePage));
-            pages.Add(GetHtmlTemplate(firstPage));
             pages.Add(GetHtmlTemplate(secondPage));
 
+            if (marketResearch != null)
+            {
+                var thirdPage = @$"
+                    <div>
+                        <h3>Studiul pieței</h3>
+                        <p><i>Există o dorință pentru produsul sau serviciul dvs?</i></p>
+                        <p>{marketResearch.Demand}</p>
+                        <p><i>Câte persoane sunt interesate de ceea ce oferă firma ta?</i></p>
+                        <p>{marketResearch.Interest}</p>
+                        <p><i>La ce persoane poate ajunge business-ul tău?</i></p>
+                        <p>{marketResearch.BusinessArea}</p>
+                        <p><i>Ce alte produse similare există? Care este competiția ta?</i></p>
+                        <p>{marketResearch.Competitors}</p>
+                        <p><i>Cât platesc potențialii clienți ale acestor produse alternative?</i></p>
+                        <p>{marketResearch.Revenue}</p>
+                    </div>
+                ";
+                pages.Add(GetHtmlTemplate(thirdPage));
+            }
+
+            if (socialMedia != null)
+            {
+                var fourthPage = @$"
+                    <div>
+                        <h3>Rețele sociale</h3>
+                        <a href='{socialMedia.Website}'><p>Pagina de Web: {socialMedia.Website}</p></a>
+                        <a href='{socialMedia.Facebook}'><p>Facebook: {socialMedia.Facebook}</p></a>
+                        <a href='{socialMedia.Instagram}'><p>Instagram: {socialMedia.Instagram}</p></a>
+                        <a href='{socialMedia.Linkedin}'><p>LinkedIn: {socialMedia.Linkedin}</p></a>
+                    </div>
+                ";
+
+                pages.Add(GetHtmlTemplate(fourthPage));
+            }
+            
             return pages;  
         }
 
