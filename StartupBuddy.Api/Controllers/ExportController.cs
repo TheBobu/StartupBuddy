7edc44a73f71f0b504f8c0e5e33a4cd2bd0c1efc
Andrei Florescu <andrei.florescu.00@gmail.com>
2022-09-29 20:20:12 +0300

    Generate zip with pdf and image

---
+++ StartupBuddy.Api/Controllers/ExportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StartupBuddy.BusinessLogic.Interfaces;
+using StartupBuddy.Dtos.File;
+
+namespace StartupBuddy.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportController : Controller
+    {
+        public ExportController(IExportBusinessLogic exportBusinessLogic)
+        {
+            ExportBusinessLogic = exportBusinessLogic;
+        }
+
+        public IExportBusinessLogic ExportBusinessLogic { get; }
+
+        [HttpGet("GenerateArchive")]
+        public async Task<FileDto> GenerateZip()
+        {
+            return await ExportBusinessLogic.ExportToZip();
+        }
+    }
+}
